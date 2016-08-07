db2f5af6c5079c3e5634ad10e41c3b96dee4d55e
c <jjuni0108@nate.com>
2016-08-07 17:20:23 +0900

    jsondata

---
+++ PhotoGallery/app/src/main/java/com/example/c/photogallery/FlickrFetchr.java
@@ -0,0 +1,91 @@
+package com.example.c.photogallery;
+
+import android.net.Uri;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by c on 2016-08-07.
+ */
+public class FlickrFetchr {
+    private static final String API_KEY = "61d85f40793d1fef143ada94ecc2bdd4";
+
+    public byte[] getUrlBytes(String urlSpec) throws IOException {
+
+
+        HttpURLConnection connection = null;
+        try {
+            URL url = new URL(urlSpec);
+            connection = (HttpURLConnection) url.openConnection();
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            InputStream in = connection.getInputStream();
+            if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                throw new IOException(connection.getResponseMessage() + " with " + urlSpec);
+            }
+            int bytesRead = 0;
+            byte[] buffer = new byte[1024];
+            while ((bytesRead = in.read(buffer)) > 0) {
+                out.write(buffer, 0, bytesRead);
+            }
+            out.close();
+            return out.toByteArray();
+        } finally {
+            connection.disconnect();
+        }
+    }
+
+    public String getUrlString(String urlSpec) throws IOException {
+        return new String(getUrlBytes(urlSpec));
+    }
+
+    public void fetchItems() {
+        ArrayList<GalleryItem> items=new ArrayList<GalleryItem>();
+        String url = Uri.parse("https://api.flickr.com/services/rest/")
+                .buildUpon()
+                .appendQueryParameter("method", "flickr.photos.getRecent")
+                .appendQueryParameter("api_key", API_KEY)
+                .appendQueryParameter("format", "json")
+                .appendQueryParameter("nojsoncallback", "1")
+                .appendQueryParameter("extras", "url_s")
+                .build().toString();
+        try {
+            String jsonString = getUrlString(url);
+            Log.d("json", jsonString);
+
+            JSONObject jsonBody = new JSONObject(jsonString);
+            parseItem(items,jsonBody);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void parseItem(List<GalleryItem> items, JSONObject jsonBody) throws JSONException {
+        JSONObject photosJsonObject = jsonBody.getJSONObject("photos");
+        JSONArray photoJsonArray = photosJsonObject.getJSONArray("photo");
+        for (int i = 0; i < photoJsonArray.length(); i++) {
+            JSONObject photoJsonObject = photoJsonArray.getJSONObject(i);
+            GalleryItem item= new GalleryItem();
+            item.setId(photoJsonObject.getString("id"));
+            item.setCaption(photoJsonObject.getString("title"));
+            if(photoJsonObject.has("url_s")){
+                item.setUrl(photoJsonObject.getString("url_s"));
+            }
+            items.add(item);
+        }
+    }
+}
--- PhotoGallery/app/src/main/java/com/example/c/photogallery/PhotoGalleryFragment.java
+++ PhotoGallery/app/src/main/java/com/example/c/photogallery/PhotoGalleryFragment.java
@@ -1,5 +1,6 @@
 package com.example.c.photogallery;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -13,11 +14,32 @@ import android.view.ViewGroup;
  * Created by c on 2016-08-07.
  */
 public class PhotoGalleryFragment extends Fragment{
+
+
+
     RecyclerView mPhotoRecyclerView;
 
     public static PhotoGalleryFragment newInstance() {
         PhotoGalleryFragment fragment = new PhotoGalleryFragment();
         return fragment;
+    }
+//키:61d85f40793d1fef143ada94ecc2bdd4
+    class FetchItemsTask extends AsyncTask<Void,Void,Void>{
+        @Override
+        protected Void doInBackground(Void... voids) {
+            FlickrFetchr f= new FlickrFetchr();
+            f.fetchItems();
+
+            return null;
+        }
+    }
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        new FetchItemsTask().execute();
+
     }
 
     @Nullable
@@ -28,4 +50,6 @@ public class PhotoGalleryFragment extends Fragment{
         mPhotoRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(),3));
         return v;
     }
+
+
 }
