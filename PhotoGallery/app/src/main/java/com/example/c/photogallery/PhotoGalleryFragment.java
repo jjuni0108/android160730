a348a70d1c797617b8f3ced9846a99d308f940ad
c <jjuni0108@nate.com>
2016-08-07 15:46:58 +0900

    fragment

---
+++ PhotoGallery/app/src/main/java/com/example/c/photogallery/PhotoGalleryFragment.java
@@ -0,0 +1,31 @@
+package com.example.c.photogallery;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by c on 2016-08-07.
+ */
+public class PhotoGalleryFragment extends Fragment{
+    RecyclerView mPhotoRecyclerView;
+
+    public static PhotoGalleryFragment newInstance() {
+        PhotoGalleryFragment fragment = new PhotoGalleryFragment();
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v =inflater.inflate(R.layout.fragment_photo_gallery,container,false);
+        mPhotoRecyclerView =(RecyclerView)v.findViewById(R.id.photo_gallery_recyclerview);
+        mPhotoRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(),3));
+        return v;
+    }
+}
