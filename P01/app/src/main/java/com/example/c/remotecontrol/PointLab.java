699cb5ca44885fecbe26650656d48e04048f4f5a
c <jjuni0108@nate.com>
2016-08-07 10:04:58 +0900

    데이터

---
+++ P01/app/src/main/java/com/example/c/remotecontrol/PointLab.java
@@ -0,0 +1,10 @@
+package com.example.c.remotecontrol;
+
+/**
+ * Created by c on 2016-08-07.
+ */
+public class PointLab {
+    private static PointLab pointLab;
+
+
+}
--- P01/app/src/main/java/com/example/c/remotecontrol/SQLite/PointSystemOpenHelper.java
+++ P01/app/src/main/java/com/example/c/remotecontrol/SQLite/PointSystemOpenHelper.java
@@ -4,18 +4,27 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import static com.example.c.remotecontrol.SQLite.PointSystemDbSchema.*;
+
 /**
  * Created by c on 2016-08-07.
  */
 public class PointSystemOpenHelper extends SQLiteOpenHelper {
+    private static final int VERSION = 1;
+    private static final String DATABASE_NAME = "PointSystem.db";
 
-    public PointSystemOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+    public PointSystemOpenHelper(Context context) {
+        super(context, DATABASE_NAME, null , VERSION);
     }
 
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table " + PointSystemTable.NAME + "(" +
+                "_id integer primary key autoincrement," +
+                PointSystemTable.Cols.UUID + "," +
+                PointSystemTable.Cols.BEVERAGE + "," +
+                PointSystemTable.Cols.DATE + "," +
+                PointSystemTable.Cols.TEL + ")");
     }
 
     @Override
